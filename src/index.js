e6f4ee63cf76f6336a4087e04fec6456a06ac2c1
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-05-12 15:08:02 -0400

    Initialized project.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Potlukk from './components/potlukk/potlukk';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Potlukk />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
